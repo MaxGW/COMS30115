8ea9c828b7d92abe7f17d4070dba65dc4fb1a15b
Max <maxgw2@yahoo.co.uk>
2019-04-02 17:14:28 +0100

    raytracer 50%, rasta started Pixel

--- Labs/rasteriser/Source/skeleton.cpp
+++ Labs/rasteriser/Source/skeleton.cpp
@@ -9,6 +9,8 @@ using namespace std;
 using glm::vec3;
 using glm::mat3;
 using glm::vec4;
+using glm::ivec2;
+using glm::vec2;
 using glm::mat4;
 
 SDL_Event event;
@@ -17,43 +19,302 @@ SDL_Event event;
 #define SCREEN_HEIGHT 256
 #define FULLSCREEN_MODE false
 
+//struct containing inverse of shortest depth in variable zinv and co-ords (x,y)
+struct Pixel{
+  int x;
+  int y;
+  float zinv;
+};
+
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 bool Update();
 void Draw(screen* screen);
+void VertexShader( vec4& v, ivec2& p);
+void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result);
+void DrawLineSDL(screen* screen, ivec2 a, ivec2 b, vec3 color);
+void DrawPolygonEdges(screen* screen,  vector<vec4>& vertices);
+void updateRotationMatrix(float cumYaw);
+void ComputePolygonRows(vector<ivec2>& vertexPixels,vector<ivec2>& leftPixels,vector<ivec2>& rightPixels);
+void DrawPolygonRows(screen* screen, vector<ivec2>& leftPixels, vector<ivec2>& rightPixels);
+void DrawPolygon(screen* screen,  vector<vec4>& vertices);
+void InterpolateP(Pixel a, Pixel b, vector<Pixel>& result);
+
+/* ----------------------------------------------------------------------------*/
+/*   GLOBALS                                                                   */
+
+float f = SCREEN_HEIGHT;
+vec4 cameraPos( 0, 0, -3.001,1 );
+vector<Triangle> triangles;
+mat4 R;
+float yaw = 0.05;
+float cumYaw = 0;
+vector<ivec2> leftPixels();
+vector<ivec2> rightPixels();
+vec3 currentColour;
+float depthBuffer[SCREEN_HEIGHT][SCREEN_WIDTH];
 
 int main( int argc, char* argv[] )
 {
-  
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  LoadTestModel(triangles);
 
+// //______________________________________
+// //testing ComputePolygonRows function
+//   vector<ivec2> vertexPixels(3);
+//   vertexPixels[0] = ivec2(10, 5);
+//   vertexPixels[1] = ivec2( 5,10);
+//   vertexPixels[2] = ivec2(15,15);
+//   vector<ivec2> leftPixels;
+//   vector<ivec2> rightPixels;
+//   vector<ivec2> loine(11);
+//   ComputePolygonRows( vertexPixels, leftPixels, rightPixels );
+//
+//   for( uint row=0; row<leftPixels.size(); ++row ){
+//     cout << "Start: ("<< leftPixels[row].x << ","<< leftPixels[row].y << "). "
+//     << "End: ("<< rightPixels[row].x << ","<< rightPixels[row].y << "). " << endl;
+//   }
+//   return 0;
+// //__________________________________
+
+
+  //updating frame
   while ( Update())
     {
       Draw(screen);
       SDL_Renderframe(screen);
     }
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
+SDL_SaveImage( screen, "screenshot.bmp" );
 
   KillSDL(screen);
   return 0;
 }
 
-/*Place your drawing here*/
-void Draw(screen* screen)
-{
+//projects 3D point v to 2D image plane storing co-ords in given p int vector
+void VertexShader( vec4& v, ivec2& p){
+  vec4 pp;
+  //update rotation matrix R from keyboard input
+  updateRotationMatrix(cumYaw);
+  //calculate rotated and transformed matrix pp
+  pp = glm::inverse(R) * (v - cameraPos);
+
+  //store projected co-ords in p
+  p.x = (f * pp.x) / pp.z + (SCREEN_WIDTH / 2);
+  p.y = (f * pp.y) / pp.z + (SCREEN_HEIGHT / 2);
+
+}
+
+//fucntion draws a line between two given points in the given colour
+void DrawLineSDL(screen* screen, ivec2 a, ivec2 b, vec3 color){
+  ivec2 delta = glm::abs( a - b );
+  int pixels = glm::max( delta.x, delta.y ) + 1;
+  vector<ivec2> line( pixels );
+  Interpolate( a, b, line );
+
+  for(uint32_t i = 0; i < line.size(); i++){
+    PutPixelSDL(screen, line[i].x, line[i].y, color);
+  }
+}
+
+//function interpolates between two given points and outputs to reults array
+//size of given reults array determins the interpolation step size
+void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result){
+
+  int N = result.size();
+  vec2 step = vec2(b-a) / float(max(N-1,1));
+  vec2 current( a );
+
+  for( int i=0; i<N; ++i ){
+    // cout << "current = " << current.x << "," << current.y << " rounded = " << round(current).x << "," << round(current).y << "\n";
+    result[i] = round(current);
+    // cout << "i = " << i << " result = (" << result[i].x << "," << result[i].y << ")\n";
+    current += step;
+  }
+}
+
+//function interpolates with pixel inputs
+void InterpolateP(Pixel a, Pixel b, vector<Pixel>& result){
+  int N = result.size();
+  float xStep = (b.x - a.x) / float(max(N-1,1));
+  float yStep = (b.y - a.y) / float(max(N-1,1));
+  float zStep = (b.zinv - a.zinv) / float(max(N-1,1));
+  Pixel current(a);
+
+  for(int i=0; i<N; ++i){
+    result[i].x = round(current.x);
+    result[i].y = round(current.y);
+    result[i].zinv = current.zinv;
+    current.x += xStep;
+    current.y += yStep;
+    current.zinv += zStep;
+  }
+
+}
+
+/* ----------------------------------------------------------------------------*/
+/* DRAWING THE STUFF                                                           */
+
+
+//fucntion receives array of projected vertices vertexPixels of a given shape
+//function interpolates between points, outputting furthest left and right pixels into arrays
+void ComputePolygonRows(vector<ivec2>& vertexPixels,vector<ivec2>& leftPixels,vector<ivec2>& rightPixels){
+  // 1. Find max and min y-value of the polygon
+  //    and compute the number of rows it occupies.
+  int maxY = -numeric_limits<int>::max();
+  int minY = numeric_limits<int>::max();
+  int vertexNo = vertexPixels.size();
+
+  //find min and max y co-ords from given projected points
+  for(int i=0; i < vertexNo; i++){
+    if(vertexPixels[i].y > maxY ){
+      maxY = vertexPixels[i].y;
+    }
+    if(vertexPixels[i].y < minY){
+      minY = vertexPixels[i].y;
+    }
+  }
+
+  //calulate number of rows
+  int rows = maxY - minY + 1;
+
+  // 2. Resize leftPixels and rightPixels
+  //    so that they have an element for each row.
+  leftPixels.resize(rows);
+  rightPixels.resize(rows);
+  // 3. Initialize the x-coordinates in leftPixels
+  //    to some really large value and the x-coordinates
+  //    in rightPixels to some really small value.
+  //  Initialise y values
+  for( int i=0; i<rows; ++i ){
+    leftPixels[i].x  = numeric_limits<int>::max();
+    leftPixels[i].y = minY + i;
+    rightPixels[i].x = -numeric_limits<int>::max();
+    rightPixels[i].y = minY + i;
+  }
+  // 4. Loop through all edges of the polygon and use
+  //    linear interpolation to find the x-coordinate for
+  //    each row it occupies. Update the corresponding
+  //    values in rightPixels and leftPixels.
+
+  for(int i = 0; i < vertexNo; i++){
+    //find next vertex
+    int j = (i+1)%vertexNo;
+    ivec2 difference = abs(vertexPixels[i] - vertexPixels[j]);
+
+    //find number of rows between two vertices (different to ROWS)
+    // int intArraySize = abs(vertexPixels[i].y - vertexPixels[j].y) + 1;
+    //allocate vector array for interpolated co-ords
+    int intArraySize = glm::max(difference.x, difference.y) + 1;
+
+    vector<ivec2> line(intArraySize);
+    //interpolate between two projected vertices, filling results array
+    Interpolate(vertexPixels[i], vertexPixels[j], line);
+
+
+
+    for(int y = 0; y < intArraySize; y++){
+      //adjusting offset between array index and absolute co-ordinate position
+      int offset = line[y].y - minY;
+      //cout << "line = " << line[y].y << " minY = " << minY << " offset = " << offset << "\n " ;
+
+      //if smaller than the current left pixel value, must be an edge, replace value
+
+      //if larger  than right pixel value, must be an edge, replace value
+      if(line[y].x > rightPixels[offset].x){
+        rightPixels[offset].x = line[y].x;
+      }
+      if(line[y].x < leftPixels[offset].x){
+        leftPixels[offset].x = line[y].x;
+      }
+    }
+  }
+}
+
+
+
+//function draws row by row between leftPixels and rightPixels
+void DrawPolygonRows(screen* screen,  vector<ivec2>& leftPixels,  vector<ivec2>& rightPixels){
+
+  if(rightPixels.size() != leftPixels.size()){
+    cout << "leftPixels rightPixels array size doesn't match";
+  }
+  else{
+    for(uint i = 0; i < leftPixels.size(); i++){
+      //how many pixels between left and right
+      int n = rightPixels[i].x - leftPixels[i].x + 1;
+      // call PutPixelSDL for every pixel to be drawn
+      for(int j = 0; j < n; j++){
+        PutPixelSDL(screen, (leftPixels[i].x + j), leftPixels[i].y, currentColour);
+      }
+    }
+  }
+}
+
+
+
+
+
+void DrawPolygonEdges(screen* screen,  vector<vec4>& vertices ){
+  int V = vertices.size();
+  // Transform each vertex from 3D world position to 2D image position:
+  vector<ivec2> projectedVertices( V );
+  for( int i=0; i<V; ++i ){
+    VertexShader(vertices[i], projectedVertices[i]);
+  }
+  // Loop over all vertices and draw the edge from it to the next vertex:
+  for( int i=0; i<V; ++i ){
+    int j = (i+1)%V; // The next vertex
+    vec3 color( 1, 1, 1 );
+    DrawLineSDL( screen, projectedVertices[i], projectedVertices[j], color );
+  }
+}
+
+
+
+//given array of 4D points making a polygon, function projects points and draws
+void DrawPolygon(screen* screen,  vector<vec4>& vertices){
+  int V = vertices.size();
+  vector<ivec2> vertexPixels( V );
+  for( int i=0; i<V; ++i ){
+    VertexShader( vertices[i], vertexPixels[i] );
+  }
+  vector<ivec2> leftPixels;
+  vector<ivec2> rightPixels;
+  ComputePolygonRows( vertexPixels, leftPixels, rightPixels );
+  DrawPolygonRows(screen, leftPixels, rightPixels);
+
+}
+
+
+void Draw(screen* screen){
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
-    }
+
+  for( uint32_t i=0; i<triangles.size(); ++i ){
+
+    vector<vec4> vertices(3);
+    vertices[0] = triangles[i].v0;
+    vertices[1] = triangles[i].v1;
+    vertices[2] = triangles[i].v2;
+
+    currentColour = triangles[i].color;
+
+    //DrawPolygonEdges(screen, vertices);
+
+    DrawPolygon(screen, vertices);
+  }
 }
+
+//funtion updates global rotation matrix from a given yaw
+void updateRotationMatrix(float yaw){
+    R = mat4(cos(yaw) , 0, sin(yaw), 0,
+           0        , 1, 0       , 0,
+           -sin(yaw), 0, cos(yaw), 0,
+           0       , 0, 0      , 1);
+}
+
 
 /*Place updates of parameters here*/
 bool Update()
@@ -63,6 +324,7 @@ bool Update()
   int t2 = SDL_GetTicks();
   float dt = float(t2-t);
   t = t2;
+  vec4 forward = vec4(R[2][0], R[2][1], R[2][2], 1);
 
   SDL_Event e;
   while(SDL_PollEvent(&e))
@@ -77,23 +339,37 @@ bool Update()
 	    int key_code = e.key.keysym.sym;
 	    switch(key_code)
 	      {
-	      case SDLK_UP:
-		/* Move camera forward */
-		break;
-	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
-	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
-	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
+
+        //__________________________________//
+    		/* Move camera forward */
+    	      case SDLK_UP:
+              cameraPos += forward;
+          	break;
+        //__________________________________//
+        /* Move camera backwards */
+            case SDLK_DOWN:
+              cameraPos -= forward;
+    		    break;
+        //__________________________________//
+        /* Move camera left */
+    	      case SDLK_LEFT:
+              updateRotationMatrix(yaw);
+              cumYaw += yaw;
+              cameraPos = R * cameraPos;
+    		    break;
+        //__________________________________//
+        /* Move camera right */
+    	      case SDLK_RIGHT:
+              updateRotationMatrix(-yaw);
+              cumYaw -= yaw;
+              cameraPos = R * cameraPos;
+    		    break;
+
 	      case SDLK_ESCAPE:
 		/* Move camera quit */
 		return false;
 	      }
-	  }  
+	  }
     }
   return true;
 }
--- Labs/raytracer/Source/skeleton.cpp
+++ Labs/raytracer/Source/skeleton.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+      #include <iostream>
 #include <glm/glm.hpp>
 #include <SDL.h>
 #include "SDLauxiliary.h"
 #include "TestModelH.h"
 #include <stdint.h>
+#define USE_MATH_DEFINES
+#include <math.h>
 
 using namespace std;
 using glm::vec3;
@@ -13,20 +15,47 @@ using glm::mat4;
 
 SDL_Event event;
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 256
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 600
 #define FULLSCREEN_MODE false
+
+struct Intersection{
+  vec4 position;
+  float distance;
+  int triangleIndex;
+};
+
+/* ----------------------------------------------------------------------------*/
+/* Globals                                                                     */
+
+vector<Triangle> triangles;
+vec4 cameraPos(0,0,-3,1);
+float focalLength =  SCREEN_HEIGHT;
+
+//Rotation
+mat4 R;
+float yaw = 0.1;
+//Illumination
+vec4 lightPos(0, -0.5, -0.7, 1.0);
+vec3 lightColour = 14.f * vec3(1,1,1);
+vec3 indirectLight =  0.5f* vec3 (1,1,1);
+
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 bool Update();
 void Draw(screen* screen);
+vec3 FindIntersection(Triangle triangle, vec4 d, vec4 s);
+bool ClosestIntersection(vec4 s, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection);
+mat4 LookAt(vec3 from, vec3 to);
 
 int main( int argc, char* argv[] )
 {
-  
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+
+  LoadTestModel(triangles);
 
   while ( Update())
     {
@@ -34,26 +63,206 @@ int main( int argc, char* argv[] )
       SDL_Renderframe(screen);
     }
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
+  SDL_SaveImage( screen, "everton.bmp" );
 
   KillSDL(screen);
   return 0;
 }
 
+vec3 FindIntersection(Triangle triangle, vec4 d, vec4 s){
+  vec4 v0 = triangle.v0;
+  vec4 v1 = triangle.v1;
+  vec4 v2 = triangle.v2;
+
+  vec3 e1 = vec3(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
+  vec3 e2 = vec3(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
+  //b = s - v0
+  vec3 b  = vec3(s.x - v0.x, s.y - v0.y, s.z - v0.z);
+
+  //(-d, e1, e2)[(t, u, v)] = s - v0
+  //  A           x         = b
+  //  x = inverse(A) * b
+
+  mat3 A(vec3(-d), e1, e2);
+
+  //int detA = glm::determinant(A);
+  //float tt = (b.x*A[0][0] + b.y*A[0][1] + b.z*A[0][2]) / detA;
+
+  /* mat3 tempA = A;
+
+  for(int i = 0; i<3; i++){
+    tempA[i][0] = b[i];
+  }
+  float tt = glm::determinant(tempA) / glm::determinant(A);
+
+  cout << "b[i] = ("  << b[0] << ", " = v
+       << b[1] << ", "
+       << b[2] << ")" << "\n";
+  cout << "a[i] = ("  << tempA[0][0] << ", "
+       << tempA[1][0] << ", "
+       << tempA[2][0] << ")" << "\n";
+  cout << "Cramers tt = " << tt << "\n";
+  cout << "Inverse tt = "  << x.x << "\n";
+
+  if(tt < 0){
+    return vec3(-1,-1,-1);
+  }*/
+
+
+  return inverse(A) * b;
+}
+
+bool ClosestIntersection(vec4 s, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection){
+  int closestIndex = -1;
+  float closestDistance = std::numeric_limits<float>::max() - 1;
+  int isValidIntersection = 0;
+
+  //loop through each triangle testing for intersection through a given ray direction
+  //for(int i = 0; i<triangles.size(); i++){
+  for(size_t i = 0; i<triangles.size(); i++){
+    vec3 x = FindIntersection(triangles[i], dir, s);
+
+    /*if(x == vec3(-1,-1,-1)Intersection lightIntersection;)  vec3 colourTemp;{
+
+      printf("***\n");
+      cout << "(" << x.x << ", "
+           << x.y << ", "
+           << x.z << ")" << "\n";
+      x = vec3(0,0,0);
+      continue;
+    }*/
+
+  //  printf("===\n");
+
+    //if valid intersection
+    if( x.y >= 0 && x.z >= 0 && (x.y + x.z) <= 1 && x.x >= 0 ){
+      //if closer than previous valid intersection update shortest distance + store index
+      if(x.x < closestDistance){
+          closestIndex = i;
+          closestDistance = x.x;
+      }
+      //variable recording the existence of a valid interesection
+      isValidIntersection = 1;
+    }
+  }
+
+  if(isValidIntersection){
+    //update global values for the closestIntersection
+    vec3 closestX = FindIntersection(triangles[closestIndex], dir, s);
+    // r = s + td
+    closestIntersection.position = s + closestX.x*dir;
+    closestIntersection.distance = closestX.x;
+    closestIntersection.triangleIndex = closestIndex;
+  }
+
+  return isValidIntersection;
+}
+
+
+
+
+vec3 DirectLight(Intersection& i){
+  Intersection lightIntersection;
+
+//  vec4 weight = float(0.01)*normalize(triangles[i.triangleIndex].normal);
+
+  //adjust intersection point along direction from i to light by scalar
+  vec4 weight = float(0.01)*((lightPos - i.position));
+  vec4 adjustedIntersection = i.position + weight;
+
+  //vector from intersection to light
+  vec4 r = lightPos - adjustedIntersection;
+  //distnace between light and intersection
+  float dist = length(vec3(r));
+
+  vec3 temp_dir = normalize(vec3(r));
+  vec4 dir(temp_dir.x, temp_dir.y, temp_dir.z, 1);
+
+  //Calculating shadows
+  if(ClosestIntersection(adjustedIntersection, dir, triangles, lightIntersection)){
+    if(lightIntersection.distance < dist){
+      return vec3(0,0,0);
+    }
+  }
+
+  //B equals the amount of power hitting Intersection
+  double radiusSquared = pow(dist,2);
+  float temp = 1 / (4 * M_PI * radiusSquared);
+  vec3 B = lightColour * temp;
+  //interestion's normal vector
+  vec3 n = normalize(vec3(triangles[i.triangleIndex].normal));
+  float projection = dot(n,normalize(vec3(r)));
+
+  //return either dot product of direction vectors, or 0.
+  return (B * max(projection, float(0)));
+}
+
+
+
+
+mat4 LookAt(vec3 from, vec3 to){
+  mat4 camToWorld;
+
+  vec3 forward = glm::normalize(from - to);
+  vec3 right = glm::cross(glm::normalize(vec3(0,1,0)), forward);
+  vec3 up = glm::cross(forward, right);
+
+  camToWorld[0][0] = right.x;
+  camToWorld[0][1] = right.y;
+  camToWorld[0][2] = right.z;
+  camToWorld[1][0] = up.x;
+  camToWorld[1][1] = up.y;
+  camToWorld[1][2] = up.z;
+  camToWorld[2][0] = forward.x;
+  camToWorld[2][1] = forward.y;
+  camToWorld[2][2] = forward.z;
+
+  camToWorld[3][0] = from.x;
+  camToWorld[3][1] = from.y;
+  camToWorld[3][2] = from.z;
+
+  camToWorld[0][3] = 0;
+  camToWorld[1][3] = 0;
+  camToWorld[2][3] = 0;
+  camToWorld[3][3] = 1;
+
+  return camToWorld;
+}
+
 /*Place your drawing here*/
-void Draw(screen* screen)
-{
+void Draw(screen* screen){
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
+
+  vec3 colour;
+  Intersection closestIntersection;
+
+  for (int y=0; y<SCREEN_HEIGHT; y++){
+  //for (int y=50; y<53; y++){
+    for(int x=0; x<SCREEN_WIDTH; x++){
+    //for(int x=50; x<53; x++){
+
+      //point defines the co-ordinate in the image plane
+      vec4 point = vec4(-(x-(SCREEN_WIDTH/2)), y-(SCREEN_HEIGHT/2), -focalLength, 1.0);
+      //storing directional vector from the camera position to centre of image
+      mat4 camToWorld = LookAt(vec3(cameraPos), vec3(0,0,0));
+      //update the point with camToWorld direction and set dir equal to vector from camPos to point
+      vec3 temp_dir = normalize(vec3((camToWorld*point) - cameraPos));
+
+      vec4 dir(temp_dir.x, temp_dir.y, temp_dir.z, 1);
+      if(ClosestIntersection(cameraPos, dir, triangles, closestIntersection)){
+        vec3 colourTemp =  triangles[closestIntersection.triangleIndex].color;
+        colour = colourTemp * (DirectLight(closestIntersection) + indirectLight);
+      }
+      else{
+        colour = vec3(0,0,0);
+      }
       PutPixelSDL(screen, x, y, colour);
     }
+  }
 }
+
+
 
 /*Place updates of parameters here*/
 bool Update()
@@ -77,23 +286,78 @@ bool Update()
 	    int key_code = e.key.keysym.sym;
 	    switch(key_code)
 	      {
-	      case SDLK_UP:
+    //__________________________________//
 		/* Move camera forward */
-		break;
-	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
+	      case SDLK_UP:
+          cameraPos.z += 0.5;
+      	break;
+    //__________________________________//
+    /* Move camera backwards */
+        case SDLK_DOWN:
+          cameraPos.z -= 0.5;
+		    break;
+    //__________________________________//
+    /* Move camera left */
 	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
+
+    //cameraPos.x -= 0.5;
+          R = mat4(cos(-yaw) , 0, sin(-yaw), 0,
+                 0        , 1, 0       , 0,
+                 -sin(-yaw), 0, cos(-yaw), 0,
+                 0       , 0, 0      , 1);
+
+          cameraPos = R * cameraPos;
+		    break;
+    //__________________________________//
+    /* Move camera right */
 	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
+
+          R = mat4(cos(yaw) , 0, sin(yaw), 0,
+                 0        , 1, 0       , 0,
+                 -sin(yaw), 0, cos(yaw), 0,
+                 0       , 0, 0      , 1);
+
+          //cameraPos.x += 0.5;
+          cameraPos = R * cameraPos;
+		    break;
+    //__________________________________//
+    /* Move camera forward */
+        case SDLK_w:
+          lightPos.z += 0.2;
+        break;
+    //__________________________________//
+    /* Move camera backwards */
+        case SDLK_s:
+          lightPos.z -= 0.2;
+        break;
+    //__________________________________//
+    /* Move camera left */
+        case SDLK_a:
+          lightPos.x -= 0.2;
+        break;
+    //__________________________________//
+    /* Move camera right */
+        case SDLK_d:
+          lightPos.x += 0.2;
+        break;
+    //__________________________________//
+    /* Move camera up */
+        case SDLK_q:
+          lightPos.y += 0.2;
+        break;
+        //__________________________________//
+    /* Move camera down  */
+        case SDLK_e:
+          lightPos.y -= 0.2;
+        break;
+    //__________________________________//
+    /* Move camera quit */
 	      case SDLK_ESCAPE:
-		/* Move camera quit */
+        break;
+
 		return false;
 	      }
-	  }  
+	  }
     }
   return true;
 }
